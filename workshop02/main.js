38143e51806ef397444a86ef034222be19d312d6
Rhea Pestano <rhea.pestano@gmail.com>
2019-07-16 16:56:22 +0800

    changes for workshop2

--- workshop02/main.js
+++ workshop02/main.js
@@ -19,25 +19,85 @@ const db = CitiesDB({
 
 const app = express();
 
+app.set('etag', false);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Start of workshop
 
 // Mandatory workshop
-// TODO GET /api/states
+app.get('/api/states', (req, res) => {
+	res.type('application/json');
+	db.findAllStates()
+		.then(result => {
+			res.status(200);
+			res.json(result);
 
+		})
+		.catch(error => {
+			res.status(400); 
+            res.send({ error : error }); 
+            return;
+		});
+
+});
 
 
 
 // TODO GET /api/state/:state
+app.get('/api/state/:state', (req, res) => {
 
+	const stateAbbr = req.params.state;
+
+	res.type('application/json');
+	db.findAllStates()
+		.then(result => {
+			if (result.indexOf(stateAbbr.toUpperCase()) < 0) {
+				res.status(400); 
+            	res.send({ error : `Not a valid state : ${stateAbbr}` }); 
+            	return;
+			}
+			const params = {
+				offset : parseInt(req.query.offset) || 10,
+				limit : parseInt(req.query.limit) || 10
+			}
+
+			return (db.findCitiesByState(stateAbbr, params))
+		}) 
+		.then(result => {
+			res.status(200);
+			res.json(result.map(v => `/api/city/${v}`));
+
+		})
+		.catch(error => {
+			res.status(400); 
+            res.send({ error : error }); 
+            return;
+		});
+
+});
 
 
 
 // TODO GET /api/city/:cityId
+app.get('/api/city/:cityId', (req, res) => {
 
+	const cityId = req.params.cityId;
 
+	res.type('application/json');
+	db.findCityById(cityId)
+		.then(result => {
+			res.status(200);
+			res.json(result);
+
+		})
+		.catch(error => {
+			res.status(400); 
+            res.send({ error : error }); 
+            return;
+		});
+
+});
 
 // TODO POST /api/city
 
